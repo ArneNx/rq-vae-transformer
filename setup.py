013f7561d287d949ed89a26b90b23b8fadbedf03
Arne Nix <arnenix@googlemail.com>
2023-08-14 09:43:15 +0200

    Made pip-installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rqvae",
+    version="0.1dev",
+    description="",
+    author="",
+    author_email="",
+    packages=["rqvae"],  # same as name
+    install_requires=[],  # external packages as dependencies
+)
